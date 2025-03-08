778abdad63557791e847ab3d546d32e15f9568dc
Herman Gohn <gohnherman@gmail.com>
2025-03-08 16:40:55 +0100

    make this go in there, use some feature mixing and matching

--- src/lib.rs
+++ src/lib.rs
@@ -322,3 +322,171 @@ impl<T> RingPtr<T> {
         self.as_mut_slice().iter_mut()
     }
 }
+pub struct CircularBuffer<T> {
+    buffer: ArrayPtr<T>,
+    cursor: usize,
+}
+impl<T> From<ArrayPtr<T>> for CircularBuffer<T> {
+    fn from(value: ArrayPtr<T>) -> Self {
+        debug_assert!(
+            !value.is_empty(),
+            "Making circular buffer from empty ArrayPtr!"
+        );
+        CircularBuffer {
+            buffer: value,
+            cursor: 0,
+        }
+    }
+}
+impl<T> CircularBuffer<T> {
+    pub fn new_zeroed(count: usize) -> CircularBuffer<T>
+    where
+        T: Zero,
+    {
+        ArrayPtr::new_zeroed(count).into()
+    }
+    pub fn new_default(count: usize) -> CircularBuffer<T>
+    where
+        T: Default,
+    {
+        ArrayPtr::new_default(count).into()
+    }
+    /// Returns what the next cursor value would be
+    fn incremented_cursor(&self) -> usize {
+        (self.cursor + 1) % (self.buffer.len())
+    }
+    fn decremented_cursor(&self) -> usize {
+        if self.cursor == 0 {
+            self.buffer.len() - 1
+        } else {
+            (self.cursor - 1) % (self.buffer.len())
+        }
+    }
+    pub fn current_as_ref(&self) -> &T {
+        unsafe { self.buffer.as_slice().get_unchecked(self.cursor) }
+    }
+    fn current_as_mut(&mut self) -> &mut T {
+        unsafe { self.buffer.as_mut_slice().get_unchecked_mut(self.cursor) }
+    }
+    /// Returns the previous value, and increments cursor
+    #[inline(always)]
+    pub fn update_forwards(&mut self, src: T) -> (T, T)
+    where
+        T: Copy,
+    {
+        let prev = *self.current_as_ref();
+        self.cursor = self.incremented_cursor();
+        let curr = replace(self.current_as_mut(), src);
+        (prev, curr)
+    }
+    pub fn update_backwards(&mut self, src: T) -> (T, T)
+    where
+        T: Copy,
+    {
+        let prev = *self.current_as_ref();
+        self.cursor = self.decremented_cursor();
+        let curr = replace(self.current_as_mut(), src);
+        (prev, curr)
+    }
+    #[inline(always)]
+    pub const fn set_cursor(&mut self, val: usize) {
+        self.cursor = val;
+    }
+    #[inline(always)]
+    pub const fn reset_cursor(&mut self) {
+        self.set_cursor(0);
+    }
+    #[inline(always)]
+    pub const fn is_empty(&self) -> bool {
+        self.buffer.is_empty()
+    }
+
+    #[inline(always)]
+    pub const fn len(&self) -> usize {
+        self.buffer.len()
+    }
+
+    #[inline(always)]
+    pub fn as_slice(&self) -> &[T] {
+        self.buffer.as_slice()
+    }
+    #[inline(always)]
+    pub fn as_mut_slice(&mut self) -> &mut [T] {
+        self.buffer.as_mut_slice()
+    }
+}
+impl<T> Index<usize> for CircularBuffer<T> {
+    type Output = <ArrayPtr<T> as Index<usize>>::Output;
+    fn index(&self, index: usize) -> &Self::Output {
+        &self.buffer[index]
+    }
+}
+impl<T> IndexMut<usize> for CircularBuffer<T> {
+    fn index_mut(&mut self, index: usize) -> &mut Self::Output {
+        &mut self.buffer[index]
+    }
+}
+
+#[cfg(feature = "circular-buffer-fft")]
+impl<T> CircularBuffer<num_complex::Complex<T>> {
+    #[inline(always)]
+    pub fn inplace_fourier_transform(
+        &mut self,
+        transformer: &dyn rustfft::Fft<T>,
+        scratch: &mut [num_complex::Complex<T>],
+    ) where
+        T: rustfft::FftNum,
+    {
+        transformer.process_with_scratch(self.buffer.as_mut_slice(), scratch);
+    }
+    pub fn inplace_hilbert_transform<'s>(
+        &'s mut self,
+        processor: &dyn rustfft::Fft<T>,
+        reverse_processor: &dyn rustfft::Fft<T>,
+        scratch: &mut [num_complex::Complex<T>],
+    ) -> &'s [num_complex::Complex<T>]
+    where
+        T: rustfft::FftNum + num_traits::NumCast,
+        num_complex::Complex<T>: core::ops::MulAssign + core::ops::DivAssign<T>,
+    {
+        self.inplace_fourier_transform(processor, scratch);
+
+        self.apply_hilbert_filter();
+
+        self.inplace_fourier_transform(reverse_processor, scratch);
+        // Ensure properly scaled values following IFFT
+        self.batch_downscale();
+
+        self.as_slice()
+    }
+    #[inline(always)]
+    pub fn apply_hilbert_filter(&mut self)
+    where
+        T: rustfft::FftNum + num_traits::NumCast + Zero,
+        num_complex::Complex<T>: core::ops::MulAssign,
+    {
+        use num_complex::Complex;
+
+        let n = self.buffer.as_slice().len();
+        for (i, elt) in self.buffer.iter_mut().enumerate().take(n) {
+            let multiplier = match i {
+                0 => Complex::zero(),                                 // Zero DC
+                i if i < n / 2 => Complex::new(T::zero(), -T::one()), // -i for positive frequencies
+                _ => Complex::zero(),                                 // Zero negative frequencies
+            };
+            *elt *= multiplier;
+        }
+    }
+    #[inline(always)]
+    fn batch_downscale(&mut self)
+    where
+        T: num_traits::Num + num_traits::NumCast + Copy,
+        num_complex::Complex<T>: core::ops::DivAssign<T>,
+    {
+        let n = self.buffer.len();
+        let n_t: T = num_traits::NumCast::from(n).expect("A usize for downscaling");
+        for elt in self.buffer.iter_mut() {
+            *elt /= n_t;
+        }
+    }
+}
